6aa9484b9d055642868e3a57feb9a1d1e69d5048
TonySun <pearlgrow@outlook.com>
2023-11-04 02:47:50 +0800

    Update wechat_manager.py

--- wxcloudrun/wechat_manager.py
+++ wxcloudrun/wechat_manager.py
@@ -4,6 +4,7 @@ import requests
 class WeChatManager:
     def send_text_message(self, open_id, message):
         url = f"http://api.weixin.qq.com/cgi-bin/message/custom/send"
+        headers = {'Content-Type': 'application/json'}
         message_data = {
             "touser": open_id,
             "msgtype": "text",
@@ -11,5 +12,5 @@ class WeChatManager:
                 "content": message
             }
         }
-        response = requests.post(url, json=message_data)
+        response = requests.post(url, headers=headers, json=message_data)
         return response.json()  # 这个调用将返回微信API的响应，您可能想要检查这个来确保消息已发送
