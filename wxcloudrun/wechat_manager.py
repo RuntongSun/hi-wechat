60c6f3d1832c084c18e47dd6634e9e419f68b07e
TonySun <pearlgrow@outlook.com>
2023-11-04 02:52:13 +0800

    Update wechat_manager.py

--- wxcloudrun/wechat_manager.py
+++ wxcloudrun/wechat_manager.py
@@ -12,5 +12,6 @@ class WeChatManager:
                 "content": message
             }
         }
-        response = requests.post(url, headers=headers, json=message_data)
+        json_message = message_data.dumps(message, ensure_ascii=False)
+        response = requests.post(url, headers=headers, json=json_message)
         return response.json()  # 这个调用将返回微信API的响应，您可能想要检查这个来确保消息已发送
