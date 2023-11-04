094f06ae72327f75e259522ed183d9e5f798b722
TonySun <pearlgrow@outlook.com>
2023-11-05 01:50:29 +0800

    Update wechat_manager.py

--- wxcloudrun/wechat_manager.py
+++ wxcloudrun/wechat_manager.py
@@ -15,7 +15,7 @@ class WeChatManager:
             }
         }
         # json_message = message_data.dumps(message, ensure_ascii=False)
-        response = requests.post(url, headers=headers, data=json.dumps(message_data, ensure_ascii=False).encode('utf-8'))
+        response = requests.post(url, headers=headers, data=json.dumps(message_data, ensure_ascii=False).encode('utf-8'), verify=False)
         return response.json()  # 这个调用将返回微信API的响应，您可能想要检查这个来确保消息已发送
 
     def send_image_message(self, user_id, media_id):
@@ -27,7 +27,7 @@ class WeChatManager:
                 "media_id": media_id
             }
         }
-        response = requests.post(url, json=message_data)
+        response = requests.post(url, json=message_data, verify=False)
         return response.json()
 
     def upload_image_data(self, image_data):
