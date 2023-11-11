43b82156d8f68bcce2112ed65c0e109d8e7bc2d6
TonySun <pearlgrow@outlook.com>
2023-11-12 03:33:25 +0800

    0.0.10

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -26,7 +26,7 @@ end_point = "http://1647939067643291.mns.cn-shanghai.aliyuncs.com"
 queue_name = "wechat-msg"  # 你的MNS队列名称
 # 初始化MNS账户和队列
 mns_account = Account(os.environ.get("ALI_MNS_ENDPOINT"), os.environ.get("ALI_KEY_ID"), os.environ.get("ALI_ACCESS_KEY_SECRET"))
-mns_queue = mns_account.get_queue(queue_name)  
+mns_queue = mns_account.get_queue(queue_name)
 
 
 @app.route('/from-aliyun', methods=['POST'])
