e5ca77266a53fb8285bf4ebcf7fc9001c1862545
TonySun <pearlgrow@outlook.com>
2023-11-14 11:33:04 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -103,25 +103,25 @@ def send_image():
 
 
 @app.route('/send_audio', methods=['POST'])
-def send_voice():
+def send_audio():
     if request.content_type == 'application/json':
-        feedback_data = request.get_json()
-        if not feedback_data:
-            return jsonify({"error": "Invalid JSON data"}), 400
+        # feedback_data = request.get_json()
+        # if not feedback_data:
+        #     return jsonify({"error": "Invalid JSON data"}), 400
+        #
+        # open_id = feedback_data.get("touser")
+        # msg_type = feedback_data.get("msgtype")
+        # media_id = feedback_data.get("media_id")
+        #
+        # if not all([open_id, msg_type, media_id]):
+        #     return jsonify({"error": "Missing required fields"}), 400
+        #
+        # try:
+        #     wechat_response = wechat_manager.send_voice_message(open_id, media_id)
+        # except Exception as e:
+        #     return jsonify({"error": str(e), 'wechat_response': wechat_response}), 500
 
-        open_id = feedback_data.get("touser")
-        msg_type = feedback_data.get("msgtype")
-        media_id = feedback_data.get("media_id")
-
-        if not all([open_id, msg_type, media_id]):
-            return jsonify({"error": "Missing required fields"}), 400
-
-        try:
-            wechat_response = wechat_manager.send_voice_message(open_id, media_id)
-        except Exception as e:
-            return jsonify({"error": str(e), 'wechat_response': wechat_response}), 500
-
-        return jsonify({"success": True}), 200
+        return jsonify({"success": 'True'}), 200
 
     abort(415)  # Unsupported Media Type
 
@@ -136,68 +136,8 @@ def wechat():
         message = Message(action='FORWARD_MESSAGE', data=json_data)
 
         response_from_aliyun = communication_manager.send_request(message)
-        # try:
-        #     # message_body = json_data  # 获取原始消息内容，这已经是一个字符串了
-        #     send_to_logger(json_data)
-        #     message = Message(message_body=json_data)  # 直接使用字符串，不需要编码为字节
-        #     re_msg = mns_queue.send_message(message)
-        #     print("Message sent to MNS. Message ID: ", re_msg.message_id)
-        #     send_to_logger(re_msg)
-        # except Exception as e:
-        #     print("Failed to send message to MNS:", e)
-        #     send_to_logger(e)
 
         return "success"
-        # if response_from_aliyun:
-        # response_str = json.dumps(response_from_aliyun, ensure_ascii=False)
-        # wechat_response = wechat_manager.send_text_message("oxi2qjn7b7rtE2rjT6TudqzqEXDs", "ok")
-        # message = Message(action='WECHAT_BACK', data=wechat_response)
-        # msg_type = json_data.get('MsgType')
-        #
-        # if msg_type in ['image', 'voice']:
-        #     media_id = json_data.get('MediaId')  # 获取媒体文件ID
-        #     media_url = f"{WECHAT_MEDIA_URL}?media_id={media_id}"
-        #
-        #     # 获取实际的媒体内容
-        #     media_response = requests.get(media_url, stream=True)
-        #     if media_response.status_code == 200:
-        #         files = {'media': media_response.content}
-        #         # 将媒体文件以及原始的JSON数据一起转发到阿里云服务器
-        #         response_from_aliyun = requests.post(ALIYUN_SERVER_URL, files=files,
-        #                                              data={'json_data': jsonify(json_data).data})
-        #
-        #         if response_from_aliyun.status_code == 200:
-        #             return response_from_aliyun.content, 200, {'ContentType': 'application/json'}
-        #         else:
-        #             # 错误处理
-        #             return "Error", 500
-        #     else:
-        #         return "Error", 500  # 媒体文件获取失败处理
-        # else:
-        #
-        #     headers = {
-        #         'Content-Type': 'application/json; charset=UTF-8'
-        #     }
-        #     response_from_aliyun = requests.post(ALIYUN_SERVER_URL, json=json_data, headers=headers)
-        #
-        #     if response_from_aliyun.status_code == 200:
-        #         response_data = response_from_aliyun.json()
-        #         response_str = json.dumps(response_data, ensure_ascii=False)
-        #         return response_str, 200, {'ContentType': 'application/json'}
-        #     else:
-        #         return "Error", 500
-
-
-def send_to_logger(json_data):
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8'
-    }
-    response = requests.post("https://g362909r31.goho.co/logger", json=json_data, headers=headers)
-
-    if response.status_code == 200:
-        print('ok')
-    else:
-        print('no')
 
 
 @app.route('/')
